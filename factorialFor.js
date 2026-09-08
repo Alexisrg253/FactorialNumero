b18c796effa3c3850459e640c01b67f97b712c38
alexis <ajrg253@gmail.com>
2026-09-08 11:17:48 -0600

    factorial iterativo2

---
+++ factorialFor.js
@@ -0,0 +1,9 @@
+javascript
+let numero = 5;
+let factorial = 1;
+
+for (let i = 1; i <= numero; i++) {
+    factorial = factorial * i;
+}
+
+console.log("El factorial de " + numero + " es: " + factorial); 
